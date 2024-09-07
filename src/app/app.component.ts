eb84eb041696055a994b61278c9f1ec03e8d8af7
Edward Bennett <44945949+ebenne01@users.noreply.github.com>
2024-09-06 20:16:43 -0400

    Demo particles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+declare var particlesJS: any;
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'hello-angular';
+  title = "hello-angular";
+
+  ngOnInit(): void {
+    particlesJS.load("particles", "particles.json", () => {
+      console.log("particles.js config loaded");
+    });
+  }
 }
